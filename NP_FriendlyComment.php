8dd25cdcb1cd226da5e355b7ff37c9d9f22bb157
yamamoto <yamamoto@kyms.jp>
2016-08-11 21:37:02 +0900

    First commit
    http://nucleuscms.org/wiki/doku.php/friendlycomment

---
+++ NP_FriendlyComment.php
@@ -0,0 +1,53 @@
+<?php
+ 
+class NP_FriendlyComment extends NucleusPlugin {
+ 
+   function getName() {
+      return 'Friendly Comment';
+   }
+ 
+   function getAuthor()  {
+      return 'Lord Matt';
+   }
+ 
+   function getURL()
+   {
+      return 'http://lordmatt.co.uk';
+   }
+ 
+   function getVersion() {
+      return '1.0';
+   }
+ 
+   function getDescription() {
+      return 'Stops data being brashly shown (such as email adresses).';
+   }
+ 
+   function getEventList() { return array('PreComment'); }
+ 
+   function event_PreComment(&$data){
+      Global $manager;
+ 
+      $pos = strpos($data['comment']['userlinkraw'], '@');
+ 
+      if ($pos === false) {
+         // do nothing it is a link and that's cool
+      } else {
+         $created = false;
+         $url = '';
+         $params = array('itemid'=>$data['comment']['itemid'], 'blogid'=>$data['comment']['blogid']);
+         $manager->notify(
+            'GenerateURL',
+            array(
+               'type' => 'item',
+               'params' => $params,
+               'completed' => &$created,
+               'url' => &$url
+            )
+         );         
+         $data['comment']['userlinkraw'] = $url . '#' . $data['commentid'];         
+      }
+ 
+   }
+ 
+}
