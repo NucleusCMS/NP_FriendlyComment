453fc7826e26c7e61e4700b8bf51f94e73fad1c2
yamamoto <yamamoto@kyms.jp>
2016-08-11 21:46:38 +0900

    Refactor

--- NP_FriendlyComment.php
+++ NP_FriendlyComment.php
@@ -1,53 +1,30 @@
 <?php
- 
 class NP_FriendlyComment extends NucleusPlugin {
- 
-   function getName() {
-      return 'Friendly Comment';
-   }
- 
-   function getAuthor()  {
-      return 'Lord Matt';
-   }
- 
-   function getURL()
-   {
-      return 'http://lordmatt.co.uk';
-   }
- 
-   function getVersion() {
-      return '1.0';
-   }
- 
-   function getDescription() {
-      return 'Stops data being brashly shown (such as email adresses).';
-   }
- 
-   function getEventList() { return array('PreComment'); }
- 
-   function event_PreComment(&$data){
-      Global $manager;
- 
-      $pos = strpos($data['comment']['userlinkraw'], '@');
- 
-      if ($pos === false) {
-         // do nothing it is a link and that's cool
-      } else {
-         $created = false;
-         $url = '';
-         $params = array('itemid'=>$data['comment']['itemid'], 'blogid'=>$data['comment']['blogid']);
-         $manager->notify(
+    function getName()        { return 'Friendly Comment';}
+    function getAuthor()      { return 'Lord Matt';}
+    function getURL()         { return 'http://lordmatt.co.uk';}
+    function getVersion()     { return '1.0';}
+    function getDescription() { return 'Stops data being brashly shown (such as email adresses).';}
+    function getEventList()   { return array('PreComment');}
+    
+    function event_PreComment(&$data){
+        global $manager;
+        
+        $pos = strpos($data['comment']['userlinkraw'], '@');
+        if ($pos === false) return;
+        
+        $created = false;
+        $url = '';
+        $params = array('itemid'=>$data['comment']['itemid'], 'blogid'=>$data['comment']['blogid']);
+        $manager->notify(
             'GenerateURL',
             array(
-               'type' => 'item',
-               'params' => $params,
-               'completed' => &$created,
-               'url' => &$url
+            'type' => 'item',
+            'params' => $params,
+            'completed' => &$created,
+            'url' => &$url
             )
-         );         
-         $data['comment']['userlinkraw'] = $url . '#' . $data['commentid'];         
-      }
- 
-   }
- 
+        );
+        $data['comment']['userlinkraw'] = $url . '#' . $data['commentid'];
+    }
 }
